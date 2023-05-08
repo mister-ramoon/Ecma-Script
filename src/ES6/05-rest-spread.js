4c1a05575c2211634208dcd8d6099e877e02d4ad
ramonruiz <jruiz@lightia.mx>
2023-05-08 15:44:21 -0600

    ES6: spread operator

--- src/ES6/05-rest-spread.js
+++ src/ES6/05-rest-spread.js
@@ -12,3 +12,23 @@ let user = {
 
 let { username, age, country } = user;
 console.log(username, age, user.country);
+
+// Spread operator
+let person = {
+  name: "Oscar",
+  age: 32,
+};
+
+let countryPerson = "MX";
+
+let data = { id: 1, ...person, countryPerson };
+console.log(data);
+
+// Rest
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3, 4, 5);
