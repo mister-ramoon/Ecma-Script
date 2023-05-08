472f6be453d703a336fad23fa44d942659b6313d
ramonruiz <jruiz@lightia.mx>
2023-05-08 11:39:37 -0600

    ES6: Strings

---
+++ src/ES6/02-strings.js
@@ -0,0 +1,19 @@
+let hello = "Hello";
+let world = "World";
+
+let epicPhrase = hello + " " + world;
+console.log(epicPhrase);
+
+let helloWorld = `${hello} ${world}`;
+console.log(helloWorld);
+
+// Multilinea
+let lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n" +
+  "Otra frase epica que necesitamos.";
+
+let lorem2 = `Otra frase epica que necesitamos 
+ahora es otra frase epica`;
+
+console.log(lorem);
+console.log(lorem2);
