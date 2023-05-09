2ac87056208317ea2561a674ee2419d274cf2d18
ramonruiz <jruiz@lightia.mx>
2023-05-09 11:44:11 -0600

    ES8: object entries y object values

---
+++ src/ES8/00-object-entries.js
@@ -0,0 +1,10 @@
+// Object Entries
+const countries = {
+  MX: "Mexico",
+  CO: "Colombia",
+  AR: "Argentina",
+  CL: "Chile",
+  PE: "Peru",
+};
+
+console.log(Object.entries(countries));
---
+++ src/ES8/01-object-values.js
@@ -0,0 +1,10 @@
+//Object Values
+const countries = {
+  MX: "Mexico",
+  CO: "Colombia",
+  AR: "Argentina",
+  CL: "Chile",
+  PU: "Peru",
+};
+
+console.log(Object.values(countries));
