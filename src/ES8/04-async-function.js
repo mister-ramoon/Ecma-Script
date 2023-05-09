54b3497eafb3018012ea7e39216e37d96cd3cc4b
ramonruiz <jruiz@lightia.mx>
2023-05-09 12:01:29 -0600

    ES8: funciones asíncronas

---
+++ src/ES8/04-async-function.js
@@ -0,0 +1,18 @@
+// Async Function
+const fnAsync = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve("Asycn!!"), 2000)
+      : reject(new Error("Error"));
+  });
+};
+
+const anotherFn = async () => {
+  const something = await fnAsync();
+  console.log(something);
+  console.log("Hello!!");
+};
+
+console.log("Before");
+anotherFn();
+console.log("After");
