04e0f9f96d05460bd2508d2290c5dfd400fef640
ramonruiz <jruiz@lightia.mx>
2023-05-09 11:19:07 -0600

    ES7: exponentiation operator y array includes

---
+++ src/ES7/00-exponential.js
@@ -0,0 +1,3 @@
+// Exponential operator
+const data = 3 ** 4;
+console.log(data); // 81
---
+++ src/ES7/01-array-includes.js
@@ -0,0 +1,4 @@
+// Array includes
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(numbers.includes(5)); // true
+console.log(numbers.includes(0)); // false
