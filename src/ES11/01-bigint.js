f969d758575b6fa01fd1c4f170b134d060bf6d16
ramonruiz <jruiz@lightia.mx>
2023-05-10 11:31:29 -0600

    ES11: BigInt y Nullish

---
+++ src/ES11/01-bigint.js
@@ -0,0 +1,5 @@
+// Big int
+const aBigNumber = 9007199254740991n;
+const anotherBigNumber = BigInt(9007199254740991);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
---
+++ src/ES11/02-nullish.js
@@ -0,0 +1,3 @@
+const anotherNumber = 1;
+const validate = anotherNumber ?? "This is a nullish value";
+console.log(validate);
