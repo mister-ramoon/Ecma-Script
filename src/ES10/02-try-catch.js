f5dd398eab898b97f295ae3338e27c44d214a932
ramonruiz <jruiz@lightia.mx>
2023-05-10 10:41:58 -0600

    ES10: try catch y fromEntries

---
+++ src/ES10/02-try-catch.js
@@ -0,0 +1,13 @@
+// try catch
+try {
+  hello();
+} catch (error) {
+  console.log(error);
+}
+
+// ES10
+try {
+  anotherFn();
+} catch {
+  console.log("There is an error");
+}
---
+++ src/ES10/03-fromentries.js
@@ -0,0 +1,5 @@
+const entries = new Map([
+  ["name", "Oscar"],
+  ["age", 32],
+]);
+console.log(Object.fromEntries(entries));
